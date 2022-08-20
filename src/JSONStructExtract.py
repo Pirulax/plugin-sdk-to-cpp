ff6d207c03fe41ed1490706f16fca8617a939bfa
Pirulax <patrikjankovics7@gmail.com>
2022-08-20 02:43:18 +0200

    Fix structinfo

--- src/JSONStructExtract.py
+++ src/JSONStructExtract.py
@@ -22,7 +22,12 @@ def extract(cls_name : str) -> StructInfo:
 
     file_path = DATABASE_PATH / 'structs' / f'gtaout.{cls_name}.json'
     if not file_path.exists():
-        return ([], 0) # Static class
+        return StructInfo(
+            variables=[], 
+            size=None,
+            vtbl_size=None,
+            vtbl_addr=None
+        )
 
     def get_data_int(key, base = 10) -> Optional[int]:
         match data.get(key, None):
@@ -35,8 +40,8 @@ def extract(cls_name : str) -> StructInfo:
     
     with file_path.open(mode='r', encoding='UTF-8') as file:
         data = json.load(file)
-        return {
-            "variables": [
+        return StructInfo(
+            variables=[
                 Variable(
                     address=member['offset'],
                     full_name=member['name'],
@@ -44,7 +49,7 @@ def extract(cls_name : str) -> StructInfo:
                 )
                 for member in data['members']
             ], 
-            "size": get_data_int('size', 16),
-            "vtbl_size": get_data_int('vtableSize', 10),
-            "vtbl_addr": get_data_int('vtableAddress', 16)
-        }
+            size=get_data_int('size', 16),
+            vtbl_size=get_data_int('vtableSize', 10),
+            vtbl_addr=get_data_int('vtableAddress', 16)
+        )
